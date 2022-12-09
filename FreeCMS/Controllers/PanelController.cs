934cf5f0632236c3075f49f6f390ccfb43ed0405
mansour9897 <mansour9897@gmail.com>
2022-12-09 19:16:47 +0330

    add panel controller

---
+++ FreeCMS/Controllers/PanelController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace FreeCMS.Controllers
+{
+    [Authorize]
+    public class PanelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }            
+    }
+}
