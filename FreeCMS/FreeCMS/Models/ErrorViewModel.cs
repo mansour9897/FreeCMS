eae1948411d60f045acb93a85863f676eebcc5b2
mansour9897 <mansour9897@gmail.com>
2023-02-06 09:45:19 +0330

    add identity area

---
+++ FreeCMS/FreeCMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FreeCMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
