7cafec77ad14651dd0a0700b4b3909079fb01977
mansour9897 <mansour9897@gmail.com>
2023-03-12 22:33:20 +0330

    udate adminlte

---
+++ FreeCMS/FreeCMS.DomainModels/Identity/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FreeCMS.DomainModels.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public string? FullName => string.Format("{0} {1}", FirstName, LastName);
+    }
+}
---
+++ FreeCMS/FreeCMS/Extensions/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace FreeCMS.Extensions
+{
+    public static class StringExtensions
+    {
+        // convert -> this is sample text
+        //max length 7 
+        //to this -> this is 
+        public static string Truncate(this string value, int maxLength)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+            return value.Length <= maxLength ? value : value.Substring(0, maxLength);
+        }
+        // convert -> this is sample text
+        //max length 7 
+        //endwith ...
+        //to this -> this is ... 
+        public static string Truncate(this string value, int maxLength, string endWith)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+            return value.Length <= maxLength ? value : value.Substring(0, maxLength) + " " + endWith;
+        }
+        public static bool ToBoolean(this string value)
+        {
+            if (string.IsNullOrEmpty(value)) throw new ArgumentNullException("Your boolean value is null.");
+            if (value != "True" && value != "False") throw new ArgumentException("Your boolean value is not valid.");
+            if (value == "True") return true;
+            return false;
+        }
+    }
+}
