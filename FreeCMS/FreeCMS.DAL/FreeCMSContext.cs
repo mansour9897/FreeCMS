2b5cac682ce2572a14507fb7a71244fbc089d089
mansour9897 <mansour9897@gmail.com>
2023-10-04 16:07:38 +0330

    correct db and dbcontext

--- FreeCMS/FreeCMS.DAL/FreeCMSContext.cs
+++ FreeCMS/FreeCMS.DAL/FreeCMSContext.cs
@@ -1,6 +1,8 @@
-﻿using FreeCMS.DomainModels.Identity;
+﻿using FreeCMS.DomainModels.Cms;
+using FreeCMS.DomainModels.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 
 namespace FreeCMS.DAL
 {
@@ -11,12 +13,22 @@ namespace FreeCMS.DAL
 		{
 		}
 
+		public DbSet<Gallery> Galleries { get; set; }
+		public DbSet<GalleryItem> GalleryItems { get; set; }
+		public DbSet<Post> Posts { get; set; }
+		public DbSet<Page> Pages { get; set; }
+		public	DbSet<Topic> Topics { get; set; }
+		public DbSet<PostTopic> PostTopics { get; set; }
+		public DbSet<Slide> Slides { get; set; }
 		protected override void OnModelCreating(ModelBuilder builder)
 		{
 			base.OnModelCreating(builder);
 			// Customize the ASP.NET Identity model and override the defaults if needed.
 			// For example, you can rename the ASP.NET Identity table names and more.
 			// Add your customizations after calling base.OnModelCreating(builder);
+
+			builder.Entity<PostTopic>().HasKey(pt => new { pt.PostId, pt.TopicId});
+
 		}
 	}
 }
--- FreeCMS/FreeCMS.DomainModels/Cms/PostTopic.cs
+++ FreeCMS/FreeCMS.DomainModels/Cms/PostTopic.cs
@@ -4,7 +4,7 @@
 	{
 		public int PostId { get; set; }
 		public int TopicId { get; set; }
-		public virtual Post Post { get; set; }
-		public virtual Topic Topic { get; set; }
+		public virtual Post? Post { get; set; }
+		public virtual Topic? Topic { get; set; }
 	}
 }
