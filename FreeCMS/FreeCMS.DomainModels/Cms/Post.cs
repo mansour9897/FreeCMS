75cd10c4b7498e129ba22be7a6caded22853b120
mansour9897 <mansour9897@gmail.com>
2023-10-04 14:07:57 +0330

    add cms models
    Add Gallery class
    Add GalleryItem class
    Add Page class
    Add Post class
    Add PostTopic class
    Add SeoBase class
    Add Slide class
    Add Topic class

---
+++ FreeCMS/FreeCMS.DomainModels/Cms/Post.cs
@@ -0,0 +1,22 @@
+﻿using FreeCMS.DomainModels.Identity;
+
+namespace FreeCMS.DomainModels.Cms
+{
+	public class Post : SeoBase
+	{
+		public int Id { get; set; }
+		public DateTime CreationDate { get; set; }
+		public DateTime LastModified { get; set; }
+		public string Title { get; set; }
+		public string Summary { get; set; }
+		public string Body { get; set; }
+		public int CountView { get; set; }
+		public bool EnableComment { get; set; }
+		public bool IsPublished { get; set; }
+		public string Image { get; set; }
+
+		public int AuthorId { get; set; }
+		public virtual ApplicationUser Author { get; set; }
+		public virtual ICollection<PostTopic> PostTopics { get; set; }
+	}
+}
---
+++ FreeCMS/FreeCMS.DomainModels/Cms/Slide.cs
@@ -0,0 +1,13 @@
+﻿namespace FreeCMS.DomainModels.Cms
+{
+	public class Slide
+	{
+		public int Id { get; set; }
+		public string Text { get; set; }
+		public DateTime CreationDate { get; set; }
+		public string Image { get; set; }
+		public string Link { get; set; }
+		public int ShowPriority { get; set; }
+	}
+
+}
