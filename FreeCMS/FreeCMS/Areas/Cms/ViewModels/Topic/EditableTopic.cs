f312ef82b48fa0285dba30208b089d7679bd72a5
mansour9897 <mansour9897@gmail.com>
2023-11-11 12:31:42 +0330

    create topic controller

---
+++ FreeCMS/FreeCMS/Areas/Cms/ViewModels/Topic/EditableTopic.cs
@@ -0,0 +1,21 @@
+﻿namespace FreeCMS.Areas.Cms.ViewModels.Topic
+{
+	public class EditableTopic
+	{
+		public int Id { get; set; }
+		[Display(Name = "تاریخ ایجاد")]
+		public DateTime CreationDate { get; set; }
+		[Display(Name = "آخرین ویرایش")]
+		public DateTime LastModified { get; set; }
+		[Required(ErrorMessage = "وارد کردن نام الزامی است.")]
+		[Display(Name = "نام")]
+		public string Name { get; set; }
+
+		[Display(Name = "توضیحات")]
+		public string Description { get; set; }
+
+		[Display(Name = "والد")]
+		public int? ParentId { get; set; }
+		public Topic Parent { get; set; }
+	}
+}
