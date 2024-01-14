3dce61c076ece69c207041fafc1169effe8c3a82
mansour9897 <mansour9897@gmail.com>
2024-01-14 15:53:20 +0330

    create persian identity describer

---
+++ FreeCMS/FreeCMS/Extensions/PersianIdentityErrorDescriber.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FreeCMS.Extensions
+{
+    public class PersianIdentityErrorDescriber : IdentityErrorDescriber
+    {
+        public override IdentityError DefaultError() { return new IdentityError { Code = nameof(DefaultError), Description = $"یک خطای غیرمنتظره رخ داده است." }; }
+        public override IdentityError ConcurrencyFailure() { return new IdentityError { Code = nameof(ConcurrencyFailure), Description = "خطای همزمانی، مولفه موردنظر تغییر کرده است." }; }
+        public override IdentityError PasswordMismatch() { return new IdentityError { Code = nameof(PasswordMismatch), Description = "رمز نادرست." }; }
+        public override IdentityError InvalidToken() { return new IdentityError { Code = nameof(InvalidToken), Description = "توکن غیرمعتبر." }; }
+        public override IdentityError LoginAlreadyAssociated() { return new IdentityError { Code = nameof(LoginAlreadyAssociated), Description = "کاربری با این اطلاعات وجود دارد." }; }
+        public override IdentityError InvalidUserName(string userName) { return new IdentityError { Code = nameof(InvalidUserName), Description = $"این نام کاربری '{userName}' غیرمجاز است، نام شما تنها باید حاوی اعداد و حروف باشد." }; }
+        public override IdentityError InvalidEmail(string email) { return new IdentityError { Code = nameof(InvalidEmail), Description = $"این ایمیل '{email}' غیرمجاز است." }; }
+        public override IdentityError DuplicateUserName(string userName) { return new IdentityError { Code = nameof(DuplicateUserName), Description = $"کاربری با این نام {userName} وجود دارد." }; }
+        public override IdentityError DuplicateEmail(string email) { return new IdentityError { Code = nameof(DuplicateEmail), Description = $"این ایمیل {email} از قبل وجود دارد." }; }
+        public override IdentityError InvalidRoleName(string role) { return new IdentityError { Code = nameof(InvalidRoleName), Description = $"نام نقش {role} غیرمجاز است." }; }
+        public override IdentityError DuplicateRoleName(string role) { return new IdentityError { Code = nameof(DuplicateRoleName), Description = $"نقشی با این نام {role} وجود دارد." }; }
+        public override IdentityError UserAlreadyHasPassword() { return new IdentityError { Code = nameof(UserAlreadyHasPassword), Description = "کاربر قبلا رمز خود را مشخص کرده است." }; }
+        public override IdentityError UserLockoutNotEnabled() { return new IdentityError { Code = nameof(UserLockoutNotEnabled), Description = "عدم دسترسی برای این کاربر فعال نیست. " }; }
+        public override IdentityError UserAlreadyInRole(string role) { return new IdentityError { Code = nameof(UserAlreadyInRole), Description = $"کاربر قبلا نقش {role} را داشت." }; }
+        public override IdentityError UserNotInRole(string role) { return new IdentityError { Code = nameof(UserNotInRole), Description = $"کاربر نقش {role} را ندارد." }; }
+        public override IdentityError PasswordTooShort(int length) { return new IdentityError { Code = nameof(PasswordTooShort), Description = $"رمز شما باید حداقل {length} کاراکتر باشد." }; }
+        public override IdentityError PasswordRequiresNonAlphanumeric() { return new IdentityError { Code = nameof(PasswordRequiresNonAlphanumeric), Description = "رمز شما باید حداقل یک کاراکتر غیر الفبایی را داشته باشد." }; }
+        public override IdentityError PasswordRequiresDigit() { return new IdentityError { Code = nameof(PasswordRequiresDigit), Description = "رمز شما باید حداقل یک عدد 0-9 را داشته باشد." }; }
+        public override IdentityError PasswordRequiresLower() { return new IdentityError { Code = nameof(PasswordRequiresLower), Description = "رمز شما باید حداقل یک حرف کوچک a-z را داشته باشد." }; }
+        public override IdentityError PasswordRequiresUpper() { return new IdentityError { Code = nameof(PasswordRequiresUpper), Description = "رمز شما باید حداقل یک حرف بزرگ A-Z را داشته باشد." }; }
+    }
+
+}
